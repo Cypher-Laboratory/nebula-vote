7d8f0f763ab5910c505af93202128ca9cd2a3d58
Elli <nathan@cypherlab.org>
2025-01-25 13:45:46 +0100

    add commands descriptions

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -4,26 +4,38 @@ import { config } from './config';
 const commands = [
   new SlashCommandBuilder()
     .setName('createpoll')
-    .setDescription('Create a new poll')
+    .setDescription('Create a new poll with multiple options')
     .addStringOption(option =>
       option
         .setName('question')
-        .setDescription('The poll question')
+        .setDescription('The question you want to ask (e.g., "What\'s your favorite color?")')
         .setRequired(true)
     )
     .addStringOption(option =>
       option
         .setName('options')
-        .setDescription('Comma-separated list of options')
+        .setDescription('List your options, separated by commas (e.g., "Red, Blue, Green, Yellow")')
         .setRequired(true)
     )
     .addIntegerOption(option =>
       option
         .setName('duration')
-        .setDescription('Poll duration in minutes')
+        .setDescription('How long should the poll run? (1-43200 minutes, e.g., 60 for 1 hour)')
         .setRequired(true)
         .setMinValue(1)
-        .setMaxValue(10080) // One week maximum
+        .setMaxValue(43200) // One month maximum
+        .addChoices(
+          { name: '5 minutes', value: 5 },
+          { name: '15 minutes', value: 15 },
+          { name: '30 minutes', value: 30 },
+          { name: '1 hour', value: 60 },
+          { name: '2 hours', value: 120 },
+          { name: '6 hours', value: 360 },
+          { name: '12 hours', value: 720 },
+          { name: '1 day', value: 1440 },
+          { name: '1 week', value: 10080 },
+          { name: '1 month', value: 43200 },
+        )
     ),
 ].map(command => command.toJSON());
 
